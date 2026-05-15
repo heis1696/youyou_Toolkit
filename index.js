09b34903de4ed93b8c2a239178f37d93e218a4e7
heis1696 <1696118633@qq.com>
2026-05-16 06:20:49 +0800

    build: v1.0.149

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { createPopupShell } from './modules/app/popup-shell.js';
 import { createPublicApi } from './modules/app/public-api.js';
 
 const SCRIPT_ID = 'youyou_toolkit';
-const SCRIPT_VERSION = '1.0.148';
+const SCRIPT_VERSION = '1.0.149';
 const MENU_ITEM_ID = `${SCRIPT_ID}-menu-item`;
 const MENU_CONTAINER_ID = `${SCRIPT_ID}-menu-container`;
 const POPUP_ID = `${SCRIPT_ID}-popup`;
@@ -42,7 +42,9 @@ const appContext = {
     variableResolverModule: null,
     contextInjectorModule: null,
     toolPromptServiceModule: null,
-    toolOutputServiceModule: null
+    toolOutputServiceModule: null,
+    toolAutomationServiceModule: null,
+    toolDataProviderModule: null
   },
   caches: {
     dynamicToolPanelCache: new Map()
